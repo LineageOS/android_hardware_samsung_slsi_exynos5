31fb3958615de90a5af67e73a292fb44d24f0fe0
Dima Zavin <dima@android.com>
2012-04-03 00:35:47 -0700

    libcsc: fix android.mk
    Change-Id: I6de150f545ba3aa953cadbcdfa2bf20bc1ce3980
    Signed-off-by: Dima Zavin <dima@android.com>

--- libcsc/Android.mk
+++ libcsc/Android.mk
@@ -11,11 +11,7 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := \
 	csc.c
 
-ifeq ($(BOARD_USE_EXYNOS_OMX), true)
 OMX_NAME := exynos
-else
-OMX_NAME := sec
-endif
 
 LOCAL_C_INCLUDES := \
 	$(LOCAL_PATH)/../exynos_omx/openmax/$(OMX_NAME)_omx/include/khronos \
@@ -33,9 +29,7 @@ LOCAL_ARM_MODE := arm
 LOCAL_STATIC_LIBRARIES := libswconverter
 LOCAL_SHARED_LIBRARIES := liblog libexynosutils
 
-ifeq ($(BOARD_USE_SAMSUNG_COLORFORMAT), true)
 LOCAL_CFLAGS += -DUSE_SAMSUNG_COLORFORMAT
-endif
 
 ifeq ($(TARGET_BOARD_PLATFORM), exynos5)
 LOCAL_C_INCLUDES += \
@@ -44,13 +38,9 @@ LOCAL_CFLAGS += -DUSE_GSCALER
 LOCAL_SHARED_LIBRARIES += libexynosgscaler
 endif
 
-ifeq ($(BOARD_USE_V4L2_ION),true)
 LOCAL_CFLAGS += -DUSE_ION
-LOCAL_SHARED_LIBRARIES += libion
-endif
+LOCAL_SHARED_LIBRARIES += libion_exynos
 
-ifeq ($(BOARD_USE_EXYNOS_OMX), true)
 LOCAL_CFLAGS += -DEXYNOS_OMX
-endif
 
 include $(BUILD_SHARED_LIBRARY)
