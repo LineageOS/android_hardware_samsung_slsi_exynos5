c21bcf9f17ddc413bfc6825feffa3581d61619df
hyeonmyeong Choi <hyeon.choi@samsung.com>
2012-09-05 00:51:31 -0700

    hardware: samsung_slsi: libcamera2: Fix preview delay and recording bugs
    This patch fixed bugs that the preview is going slower when after capture.
    This patch also fixed preview color corruption when 480p recording.
    
    Change-Id: I4823830362cac17cd65a13776cd654801ab2d1fc
    Signed-off-by: hyeonmyeong Choi <hyeon.choi@samsung.com>

--- libcamera2/ExynosCameraHWInterface2.cpp
+++ libcamera2/ExynosCameraHWInterface2.cpp
@@ -2939,6 +2939,10 @@ void ExynosCameraHWInterface2::m_sensorThreadFunc(SignalDrivenThread * self)
 
             int current_scp = shot_ext->request_scp;
 
+            if (shot_ext->shot.dm.request.frameCount == 0) {
+                ALOGE("ERR(%s): dm.request.frameCount = %d", shot_ext->shot.dm.request.frameCount);
+            }
+
             cam_int_qbuf(&(m_camera_info.isp), index);
 
             usleep(10000);
@@ -3334,6 +3338,9 @@ void ExynosCameraHWInterface2::m_streamFunc0(SignalDrivenThread *self)
         nsecs_t timestamp;
         camera2_stream *frame;
 
+        int numOfUndqbuf = 0;
+        bool again = false;
+
         ALOGV("DEBUG(%s): stream(%d) processing SIGNAL_STREAM_DATA_COMING",
             __FUNCTION__,selfThread->m_index);
 
@@ -3429,6 +3436,7 @@ void ExynosCameraHWInterface2::m_streamFunc0(SignalDrivenThread *self)
                     selfRecordParms->numSvcBufsInHal--;
                 }
             }
+
             if (m_previewCbOutput && m_previewCbEnabled) {
                 ALOGV("DEBUG(%s): Entering previewcb creator, index(%d)",__FUNCTION__, selfPreviewCbParms->svcBufIndex);
 
@@ -3521,6 +3529,7 @@ void ExynosCameraHWInterface2::m_streamFunc0(SignalDrivenThread *self)
                     selfPreviewCbParms->numSvcBufsInHal--;
                 }
             }
+
             if (m_previewOutput && m_requestManager->GetSkipCnt() <= 0) {
 
                 ALOGV("** Display Preview(frameCnt:%d)", m_requestManager->GetFrameIndex());
@@ -3543,8 +3552,20 @@ void ExynosCameraHWInterface2::m_streamFunc0(SignalDrivenThread *self)
                 selfStreamParms->svcBufStatus[index] = ON_HAL;
             }
 
+            // HACK
+            if (again == false) {
+                if (exynos_v4l2_g_ctrl(currentNode->fd, V4L2_CID_IS_G_COMPLETES, &numOfUndqbuf)) {
+                    ALOGW("WARN(%s): Fail to get SCP completes, val = %d", __FUNCTION__, numOfUndqbuf);
+                } else {
+                    again = (numOfUndqbuf > 0)?true:false;
+                    if (again)
+                        ALOGW("WARN(%s): Drain SCP buf, num of undqbuf = %d", __FUNCTION__, numOfUndqbuf);
+                }
+            } else {
+                again = false;
+            }
         }
-        while (0);
+        while(again);
 
         if (m_recordOutput && m_recordingEnabled) {
             do {
@@ -3651,6 +3672,7 @@ void ExynosCameraHWInterface2::m_streamFunc0(SignalDrivenThread *self)
                     break;
                 }
             }
+
             ALOGV("DEBUG(%s): post_dequeue_buffer found(%d)", __FUNCTION__, found);
             if (!found) break;
             ALOGV("DEBUG(%s): preparing to qbuf [%d]", __FUNCTION__, checkingIndex);
